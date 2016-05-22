bc4debcc9392e4e7d4087d4265cec35d7596647e
dramaking <wzc1986@gmail.com>
2016-05-22 19:42:11 +1200

    finish home page

--- js/app.js
+++ js/app.js
@@ -8,6 +8,18 @@ var main = function() {
 			$('.site-wrapper').addClass('display-nav');
 		}
 	});
+
+	// var movementStrength = 25;
+	// var height = movementStrength / $(window).height();
+	// var width = movementStrength / $(window).width();
+	// $(".full-section").mousemove(function(e){
+ //          var pageX = e.pageX - ($(window).width() / 2);
+ //          var pageY = e.pageY - ($(window).height() / 2);
+ //          var newvalueX = width * pageX * -1 - 25;
+ //          var newvalueY = height * pageY * -1 - 50;
+ //          $('.full-section').css("background-position", newvalueX+"px     "+newvalueY+"px");
+	// });
 };
+
 
 $(document).ready(main);
