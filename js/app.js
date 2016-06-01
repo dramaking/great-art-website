abf22b7e48aba42c2e57c2b22cc4beda6e5b0914
dramaking <wzc1986@gmail.com>
2016-06-01 16:10:20 +1200

    blog navigation, solve google map problem

--- js/app.js
+++ js/app.js
@@ -4,34 +4,40 @@ var main = function() {
 
 		if ($('.site-wrapper').hasClass('display-nav')) {
 			$('.site-wrapper').removeClass('display-nav');
+			$('body').css({"overflow":"scroll"});
+			$('iframe').delay(1000).show(0);
 		} else {
 			$('.site-wrapper').addClass('display-nav');
+			$('body').css({"overflow":"hidden"});
+			$('iframe').hide();
 		}
 	});
 
-	// $('.lightbox').click(function(event) {
-	// 	$('#lightgallery').lightGallery({
-	// 		fullScreen: true,
-	// 		zoom: true,
-	// 		download: false
-	// 	});
-	// });
+	$(".blog-nav-mobile").html($(".blog-nav-container").html());
 
-	// $('#lightgallery').lightGallery({
-	// 	fullScreen: true,
-	// 	zoom: true,
-	// 	download: false
-	// });
-	// var movementStrength = 25;
-	// var height = movementStrength / $(window).height();
-	// var width = movementStrength / $(window).width();
-	// $(".full-section").mousemove(function(e){
- //          var pageX = e.pageX - ($(window).width() / 2);
- //          var pageY = e.pageY - ($(window).height() / 2);
- //          var newvalueX = width * pageX * -1 - 25;
- //          var newvalueY = height * pageY * -1 - 50;
- //          $('.full-section').css("background-position", newvalueX+"px     "+newvalueY+"px");
-	// });
+   
+    $(window).click(function(event) {
+    	if ($(".blog-nav-mobile ul").hasClass("expanded")) {
+    		$(".blog-nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+            $(".blog-nav-trigger span").removeClass("open");
+            $(".blog-nav-trigger span").removeClass("update");
+    	}
+
+    });
+
+     $(".blog-nav-trigger span").click(function(event){      
+     	event.stopPropagation();  
+        if ($(".blog-nav-mobile ul").hasClass("expanded")) {
+            $(".blog-nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+            $(this).removeClass("open");
+            $(this).removeClass("update");
+        } else {
+            $(".blog-nav-mobile ul").addClass("expanded").slideDown(250);
+            $(this).addClass("open");
+            $(this).addClass("update");
+        }
+    });
+
 };
 
 $(document).ready(main);
